bd7449419126256217e40a3c8e275ecd31419b59
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-12-13 02:29:57 +0100

    Feat(utils): Added internal method to get vue instance from the project

---
+++ packages/utils/src/internal/getVueInstance.ts
@@ -0,0 +1,17 @@
+import { getCurrentInstance } from 'vue'
+import type { Route, I18n } from './types'
+
+type VueInstance = {
+    i18n: I18n|null;
+    route: Route|null;
+};
+
+export default function (): VueInstance {
+    const instance = getCurrentInstance();
+    const globalProperties = instance?.appContext.config.globalProperties;
+
+    return {
+        i18n: globalProperties?.$i18n ?? null,
+        route: globalProperties?.$route ?? null,
+    };
+}
