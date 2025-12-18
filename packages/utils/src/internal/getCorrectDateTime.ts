99b3c2105c6b94311a9895c6d25c1f5dfcb5f205
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2025-12-18 22:21:18 +0100

    Feat(utils): Updated getCorrectDateTime to support dates in format as well

--- packages/utils/src/internal/getCorrectDateTime.ts
+++ packages/utils/src/internal/getCorrectDateTime.ts
@@ -1,5 +1,19 @@
 import { DateTime } from 'luxon'
 
 export default function (date: DateTime|string|null): DateTime {
-    return !date ? DateTime.now() : typeof date === "string" ? DateTime.fromISO(date) : date;
+    if (!date) {
+        return DateTime.now();
+    }
+
+    if (date instanceof DateTime) {
+        return date;
+    }
+
+    const isoDate = DateTime.fromISO(date);
+
+    if (isoDate.isValid) {
+        return isoDate;
+    }
+
+    return DateTime.fromFormat(date, "yyyy-MM-dd");
 }
